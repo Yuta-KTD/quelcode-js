f234950aa153bfabfeed70668d9149e91a162b38
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-09-23 08:09:15 +0900

    ページの初期値をロンドンに設定

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,61 @@
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    //選択された都市を取得
+    //初期値の設定
+    //<div>の各id要素を取得
+    var result_main = document.getElementById("result_main");
+    var icon = document.getElementById("icon");
+    var temperature = document.getElementById("temperature");
+    var humidity = document.getElementById("humidity");
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener(
+      "loadstart",
+      function () {
+        result_main.textContent = "通信中...";
+      },
+      false
+    );
+
+    xhr.addEventListener(
+      "load",
+      function () {
+        //JSONデータ取得
+        var weather_data = JSON.parse(xhr.responseText);
+        if (weather_data === null) {
+          result_main.textContent = "存在しない地域を選択しています。";
+        } else {
+          //天気情報
+          //https://openweathermap.org/weather-conditions#How-to-get-icon-URLを参考に700番台以外は日本語で天気表示
+          result_main.textContent = weather_data.weather[0].main;
+          //現在の天気の画像を取得し<img>要素に出力
+          var img = document.createElement("img");
+          var weather_icon_img = weather_data.weather[0].icon;
+          img.src =
+            "http://openweathermap.org/img/w/" + weather_icon_img + ".png";
+          img.alt = weather_data.name;
+          img.height = 100;
+          img.width = 100;
+          icon.appendChild(img);
+          //気温を摂氏にて小数点第一位まで四捨五入で表示
+          temperature.textContent =
+            Math.round((weather_data.main.temp - 273.15) * 10) / 10;
+          //湿度
+          humidity.textContent = weather_data.main.humidity;
+        }
+      },
+      false
+    );
+
+    xhr.addEventListener("error", function () {
+      result_main.textContent = "サーバーエラーが発生しました。";
+    });
+
+    //ロンドンのidをでサーバー問合せ
+    xhr.open("GET", "php/weather.php?capital_id=2643743", true);
+    xhr.send(null);
+
+    //選択された都市を取得（送信ボタンを押した時の処理）
     document.getElementById("btn").addEventListener(
       "click",
       function () {
@@ -25,8 +79,6 @@ document.addEventListener(
           function () {
             //JSONデータ取得
             var weather_data = JSON.parse(xhr.responseText);
-            //あとで消す
-            console.log(weather_data);
             if (weather_data === null) {
               result_main.textContent = "存在しない地域を選択しています。";
             } else {
@@ -71,8 +123,6 @@ document.addEventListener(
       },
       false
     );
-    //初期値の設定
-    //setListValue("capital_id", "2643743");
   },
   false
 );
