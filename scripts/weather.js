b05f1c232e0516f94f548120fff2b5b5b7090c9f
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-09-23 17:29:53 +0900

    show関数をアロー関数で実装。関数内の変数宣言も変更。

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,14 +1,12 @@
 //初期値（ロンドン）の設定
+
 document.addEventListener(
   "DOMContentLoaded",
   function () {
     //問合せURLを作成
     //show関数を呼び出すためのcallback=showをつけたURLを作成
-    $appId = "4b5774e9f3d2a07b84f0f2f88e486224";
-    var url =
-      "http://api.openweathermap.org/data/2.5/weather?callback=show&id=2643743" +
-      "&appid=" +
-      $appId;
+    const app_id = "4b5774e9f3d2a07b84f0f2f88e486224";
+    var url = `http://api.openweathermap.org/data/2.5/weather?callback=show&id=2643743&appid=${app_id}`;
     //script要素の作成
     var script = document.createElement("script");
     script.src = url;
@@ -38,17 +36,17 @@ document.getElementById("btn").addEventListener(
 );
 
 //受け取ったweather_dataを引数にコールバック関数showを実行
-function show(weather_data) {
+const show = (weather_data) => {
   if (weather_data === null) {
     result_main.textContent = "データの取得に失敗しました。";
   } else {
     //天気情報
-    capital.textContent = weather_data.name + "の現在の天気";
+    capital.textContent = `${weather_data.name} の現在の天気`;
     result_main.textContent = weather_data.weather[0].main;
     //現在の天気の画像を取得し<img>要素に出力
-    var img = document.createElement("img");
-    var weather_icon_img = weather_data.weather[0].icon;
-    img.src = "http://openweathermap.org/img/w/" + weather_icon_img + ".png";
+    const img = document.createElement("img");
+    const weather_icon_img = weather_data.weather[0].icon;
+    img.src = `http://openweathermap.org/img/w/${weather_icon_img}.png`;
     img.alt = weather_data.name;
     img.height = 60;
     img.width = 60;
@@ -64,4 +62,4 @@ function show(weather_data) {
     //湿度
     humidity.textContent = weather_data.main.humidity;
   }
-}
+};
