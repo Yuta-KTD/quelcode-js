cd973fb17a8cefb9f13584443eac416c9f37b7d7
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-09-24 14:12:59 +0900

    イベントリスナーの第２引数を関数→変数に変更

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,9 +9,9 @@ const appid = "4b5774e9f3d2a07b84f0f2f88e486224";
 //script要素を作成するcreateURL関数を作成
 const createScript = () => {
   //show関数を呼び出すためのcallback=showをつける
-  //NULL合体演算子に
+  //初期値かselectで取得した値をcapital_idとする
   const capital_id =
-    encodeURIComponent(document.getElementById("capital_id").value) ||
+    encodeURIComponent(document.getElementById("capital_id").value) ??
     capital_id_London;
   //問合せURL
   const url = `${base_url}${capital_id}&appid=${appid}`;
@@ -23,7 +23,7 @@ const createScript = () => {
   //既存の問合せ用scriptを検索
   if (document.getElementById("weather_script") !== null) {
     //すでに要素が存在する場合はscriptタグを書き換え
-    let old_weather_script = document.getElementById("weather_script");
+    const old_weather_script = document.getElementById("weather_script");
     document
       .getElementsByTagName("body")
       .item(0)
@@ -47,30 +47,21 @@ document
 
 //受け取ったweather_dataを引数にコールバック関数showを実行
 const show = (weather_data) => {
-  if (weather_data === null) {
-    result_main.textContent = "データの取得に失敗しました。";
-  } else {
-    //天気情報
-    capital.textContent = `${weather_data.name} の現在の天気`;
-    result_main.textContent = weather_data.weather[0].main;
-    //現在の天気の画像を取得し<img>要素に出力
-    const icon = document.getElementById("icon");
-    const img = document.createElement("img");
-    const weather_icon_img = weather_data.weather[0].icon;
-    img.src = `http://openweathermap.org/img/w/${weather_icon_img}.png`;
-    img.alt = weather_data.name;
-    img.height = 60;
-    img.width = 60;
-    //画像の入れ替え
-    if (icon.getElementsByTagName("img").length > 0) {
-      icon.replaceChild(img, icon.lastChild);
-    } else {
-      icon.appendChild(img);
-    }
-    //気温を摂氏にて小数点第一位まで四捨五入で表示
-    temperature.textContent =
-      Math.round((weather_data.main.temp - 273.15) * 10) / 10;
-    //湿度
-    humidity.textContent = weather_data.main.humidity;
+  if (!weather_data) {
+    return (result_main.textContent =
+      "選択範囲外の都市データを入力していませんか？");
   }
+  //天気情報
+  capital.textContent = `${weather_data.name} の現在の天気`;
+  result_main.textContent = weather_data.weather[0].main;
+  //HTMLに用意した<img>タグを取得し、要素をいれる
+  const icon = document.getElementById("icon");
+  const weather_icon_img = weather_data.weather[0].icon;
+  icon.src = `http://openweathermap.org/img/w/${weather_icon_img}.png`;
+  icon.alt = weather_data.name;
+  //気温を摂氏にて小数点第一位まで四捨五入で表示
+  temperature.textContent =
+    Math.round((weather_data.main.temp - 273.15) * 10) / 10;
+  //湿度
+  humidity.textContent = weather_data.main.humidity;
 };
